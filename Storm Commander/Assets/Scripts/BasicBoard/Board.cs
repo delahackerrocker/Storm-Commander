5212cdd1807d13c91060a9c659eea299a3c20fae
Dela <stevedelatorre@gmail.com>
2020-03-25 02:28:02 -0400

    Beginnings of Board and Cell

---
+++ Storm Commander/Assets/Scripts/BasicBoard/Board.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour
+{
+    public GameObject mCellPrefab;
+
+    [HideInInspector]
+    public Cell[,] mAllCells = new Cell[8, 8];
+
+    public void Create()
+    {
+        // Create
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                // Create the cell
+                GameObject newCell = Instantiate(mCellPrefab, transform);
+
+                // position
+                RectTransform rectTransform = newCell.GetComponent<RectTransform>();
+                rectTransform.anchoredPosition = new Vector2((x * 100) + 50, (y * 100) + 50);
+
+                // setup
+                mAllCells[x, y] = newCell.GetComponent<Cell>();
+                mAllCells[x, y].Setup(new Vector2Int(x, y), this);
+            }
+        }
+
+        // Color
+        for (int x = 0; x < 8; x += 2)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                // Offset for every other line
+                int offset = (y % 2 != 0) ? 0 : 1;
+                int finalX = x + offset;
+
+                // Color
+                mAllCells[finalX, y].GetComponent<Image>().color = new Color32(230, 220, 187, 255);
+            }
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Storm Commander/Assets/Scripts/BasicBoard/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Board mBoard;
+    void Start()
+    {
+        mBoard.Create();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
