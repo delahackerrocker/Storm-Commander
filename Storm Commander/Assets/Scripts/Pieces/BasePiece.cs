724fec533070539d3f0928de1c223495324ab423
Dela <stevedelatorre@gmail.com>
2020-04-08 08:25:21 -0400

    Chess Pieces
    Created super class and child classes for all chess pieces

---
+++ Storm Commander/Assets/Scripts/Pieces/BasePiece.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public abstract class BasePiece : EventTrigger
+{
+    /*
+    [HideInInspector]
+    public Color mColor = Color.clear;
+
+    protected Cell mOriginalCell = null;
+    protected Cell mCurrentCell = null;
+
+    protected RectTransform mRectTransform = null;
+    protected PieceManager mPieceManager;
+
+    public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
+    {
+        mPieceManager = newPieceManager;
+
+        mColor = newTeamColor;
+        GetComponent<Image>().color = newSpriteColor;
+        mRectTransform = GetComponent<RectTransform>();
+    }
+
+    public void Place(Cell newCell)
+    {
+        // Set the current cell and original cell to the newCell that was passed in
+        mCurrentCell = newCell;
+        mOriginalCell = newCell;
+        // the current cell's current piece is now equal to this piece
+        mCurrentCell.mCurrentPiece = this;
+
+        // this piece's transform position is now the cell's transform position
+        transform.position = newCell.transform.position;
+        gameObject.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
