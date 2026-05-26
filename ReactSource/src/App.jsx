53c139d258d0506f8ead373ca77e338ef7148aba
Dela <stevedelatorre@gmail.com>
2026-05-26 19:46:03 -0400

    Fixed Merge Conflicts Between iOS and Optimization Branches

--- ReactSource/src/App.jsx
+++ ReactSource/src/App.jsx
@@ -1,19 +1,42 @@
 import { useState } from 'react'
+import { IosDevicePreviewPage } from './pages/IosDevicePreviewPage'
 import { StartPage } from './pages/StartPage'
 import { StormCommanderPage } from './pages/StormCommanderPage'
 
 const PAGES = {
   start: 'start',
+  iosPreview: 'ios-preview',
   randomEncounter: 'random-encounter',
   stormChessDrill: 'storm-chess-drill',
   basicChess: 'basic-chess',
 }
 
+const STORM_VIEW_QUERY_PARAM = 'storm-view'
+const STORM_VIEW_IOS_PREVIEW = 'ios-preview'
+const STORM_VIEW_IOS_FRAME = 'ios-frame'
+
+function getStormViewMode() {
+  return new URLSearchParams(window.location.search).get(STORM_VIEW_QUERY_PARAM)
+}
+
 function App() {
-  const [currentPage, setCurrentPage] = useState(PAGES.start)
+  const stormViewMode = getStormViewMode()
+  const [currentPage, setCurrentPage] = useState(() =>
+    stormViewMode === STORM_VIEW_IOS_PREVIEW ? PAGES.iosPreview : PAGES.start,
+  )
 
   function openPage(page) {
     setCurrentPage(page)
+  }
+
+  if (stormViewMode === STORM_VIEW_IOS_FRAME) {
+    return (
+      <StormCommanderPage
+        key="storm-ios-frame-random-encounter"
+        allowChessDrill={false}
+        startInRandomEncounter
+      />
+    )
   }
 
   let page = (
@@ -31,6 +54,15 @@ function App() {
         allowChessDrill={false}
         onBack={() => openPage(PAGES.start)}
         startInRandomEncounter
+      />
+    )
+  }
+
+  if (currentPage === PAGES.iosPreview) {
+    page = (
+      <IosDevicePreviewPage
+        key="ios-device-preview"
+        onBack={() => openPage(PAGES.start)}
       />
     )
   }
